4b7beb8a2f2c97b8389715c12ac2848be8f0ad1b
Anisa <Anisa@ADA-039602>
2025-01-07 13:24:29 +0000

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesWebApp_Anisa.Models;
+
+namespace JokesWebApp_Anisa.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesWebApp_Anisa.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ Models/Joke.cs
@@ -0,0 +1,17 @@
+﻿namespace JokesWebApp_Anisa.Models
+{
+    public class Joke
+    {
+        // properties - type prop and tab2X to create a new property
+        public int ID { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        // methods - type ctror and tab2X to create a new constructor function
+        public Joke()
+        {
+                
+        }
+
+    }
+}
