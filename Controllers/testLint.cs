e0b5e0aa50fbb837a20074a1bd93f478ab4bb77b
AnisaElezi <122229106+AnisaElezi@users.noreply.github.com>
2025-01-10 10:00:30 +0000

    Update testLint.cs
    update testLint.cs under controller

--- Controllers/testLint.cs
+++ Controllers/testLint.cs
@@ -1,34 +1,34 @@
-using System;
-using Microsoft.AspNetCore.Mvc;
+// using System;
+// using Microsoft.AspNetCore.Mvc;
 
-namespace MyWebApp.Controllers
-{
-    public class HomeController : Controller
-    {
-        // Incorrect method naming convention
-        public IActionResult Index_Page()
-        {
-            // Unused variable
-            string UnusedVariable = "Hello World";
+// namespace MyWebApp.Controllers
+// {
+//     public class HomeController : Controller
+//     {
+//         // Incorrect method naming convention
+//         public IActionResult Index_Page()
+//         {
+//             // Unused variable
+//             string UnusedVariable = "Hello World";
 
-            // Missing semicolon (syntax error)
-            ViewData["Message"] = "Welcome to my web app"
+//             // Missing semicolon (syntax error)
+//             ViewData["Message"] = "Welcome to my web app"
 
-            // Incorrect return type (should be View or JsonResult)
-            return "This should be a view";
-        }
+//             // Incorrect return type (should be View or JsonResult)
+//             return "This should be a view";
+//         }
 
-        // Method with no return statement (missing return View())
-        public IActionResult About()
-        {
-            ViewData["Title"] = "About Page";
-        }
+//         // Method with no return statement (missing return View())
+//         public IActionResult About()
+//         {
+//             ViewData["Title"] = "About Page";
+//         }
 
-        // Wrong visibility modifier (should be public or private)
-        void Contact()
-        {
-            ViewData["Title"] = "Contact Page";
-            Console.WriteLine("Contact Page Loaded");
-        }
-    }
-}
+//         // Wrong visibility modifier (should be public or private)
+//         void Contact()
+//         {
+//             ViewData["Title"] = "Contact Page";
+//             Console.WriteLine("Contact Page Loaded");
+//         }
+//     }
+// }
